32ae1b2250d19c3d50efe5b3cbc53f6a606dcf9e
Eric Hartmann <Eric.Hartmann@mni.thm.de>
2011-07-22 14:17:15 +0200

    added admin-features

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  before_filter :authenticate_user!, :except => [:index, :show]
+  before_filter :is_admin?, :only => :destroy
+  
   # GET /comments
   # GET /comments.json
 
