f22ecf30427e9b2ff4ae09824cee6c1a0bd9130c
jx47 <jx47@gmx.de>
2011-07-26 14:42:34 +0200

    Merge jabber-bot to master branch

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, :presence => true
-  validates :content, :presence => true,
-    :length => { :minimum => 5 }
+  validates :content, :presence => true, :length => { :minimum => 5 }
   has_many :comments, :dependent => :destroy
+  belongs_to :author, :foreign_key => :author_id, :class_name => "User"
 end
