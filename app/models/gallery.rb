d7a9f1d9dc3830f8516e5fc005d738c30859e7ec
Charos | Dirk Grabbert <dirk.grabbert@mni.thm.de>
2011-07-29 13:36:44 +0200

    SomeThing v1.0

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,4 +1,7 @@
 class Gallery < ActiveRecord::Base
   attr_accessible :name, :visibility
   has_many :paintings, :dependent => :destroy
+
+  validates :name, :presence => true,
+    :length => { :minimum => 3, :maximum => 12}
 end
